05d7e9b7a0a2c7cac1cec3fbb03fea0ae609a27a
PunchyArchy <ksmdrmvstchny@gmail.com>
2021-04-09 15:17:53 +0500

    Добавлено создание пользователей

--- ftp_creater_linux/functions.py
+++ ftp_creater_linux/functions.py
@@ -17,19 +17,33 @@ def execute_command(command):
     os.system(command)
 
 
-def launch(username, sudo_pass):
+def launch(username, sudo_pass, subfolder='files'):
     """ Основной движок """
+    # Создать пользователя
+    create_user(username, sudo_pass)
+    # Создать ему пароль
+    set_user_pas(username, sudo_pass)
     # Создать директорию пользователя
-    create_user_folder(username, sudo_pass)
+    create_user_folder(username, sudo_pass, subfolder)
     # Ограничить его директорию
     restrict_user_folder(username, sudo_pass)
     # Даем необходимые полномочия пользователю на запись в подпапку
-    give_subfolder_credentials(username, sudo_pass)
+    give_subfolder_credentials(username, sudo_pass, subfolder)
 
 
-def create_user_folder(username, sudo_pass):
+def create_user(username, sudo_pass):
+    command = 'useradd -m -c "Created by FTP_user_creator_py" -s /bin/bash {}'.format(username)
+    get_sudo_execute(command, sudo_pass)
+
+
+def set_user_pas(username, sudo_pass):
+    command = "passwd {}".format(username)
+    get_sudo_execute(command, sudo_pass)
+
+
+def create_user_folder(username, sudo_pass, subfolder):
     """ Создать папку пользователя """
-    command = "mkdir -p /home/{}/ftp/files".format(username)
+    command = "mkdir -p /home/{}/ftp/{}".format(username, subfolder)
     get_sudo_execute(command, sudo_pass)
 
 
@@ -39,7 +53,7 @@ def restrict_user_folder(username, sudo_pass):
     get_sudo_execute(command, sudo_pass)
 
 
-def give_subfolder_credentials(username, sudo_pass, subfoldername='files'):
+def give_subfolder_credentials(username, sudo_pass, subfoldername):
     command = "chown -R {}:{} /home/{}/ftp/{}".format(username, username, username, subfoldername)
     get_sudo_execute(command, sudo_pass)
     command = "chmod -R 0770 /home/{}/ftp/{}".format(username, subfoldername)
